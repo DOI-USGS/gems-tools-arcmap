2093f183bc1ccbcaaabf6c3297ee2a961a13b359
Ralph Haugerud <rhaugerud@usgs.gov>
2020-10-07 11:12:51 -0700

    Add files via upload
    Forgot to update versionString. Fixed

--- Scripts/GeMS_ValidateDatabase_Arc10.py
+++ Scripts/GeMS_ValidateDatabase_Arc10.py
@@ -16,7 +16,7 @@ import arcpy, os, os.path, sys, time, glob
 from GeMS_utilityFunctions import *
 from GeMS_Definition import *
 
-versionString = 'GeMS_ValidateDatabase_Arc10.py, version of 7 June 2020'
+versionString = 'GeMS_ValidateDatabase_Arc10.py, version of 7 October 2020'
 debug = False
 
 metadataSuffix = '-vFgdcMetadata.txt'
