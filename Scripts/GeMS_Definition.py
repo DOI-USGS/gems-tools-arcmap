b60e9fb254dd1ef469385ed4239c62ab8a59549b
Evan Thoms <ethoms@usgs.gov>
2020-11-05 10:28:48 -0900

    Edited version string, empty spaces in GeMS_Definition.py

--- Scripts/GeMS_Definition.py
+++ Scripts/GeMS_Definition.py
@@ -6,7 +6,7 @@
 #  Ralph Haugerud, 19 August 2016 
 # 
 
-versionString = 'GeMS_Definition.py, version of 19 July 2020'
+versionString = 'GeMS_Definition.py, version of 29 October 2020'
 # fixes errors in Station feature class definition
 # 16 Jan 2014: added ObservedMapUnit to Station feature class definition
 # 8 April 2016: Fixed ObservedMapUnit.  Put _ID field at end of each field list
@@ -25,6 +25,7 @@ versionString = 'GeMS_Definition.py, version of 19 July 2020'
 # 19 July 2020: Added 'Age' to attribDict
 # 25 Sept 2020: Swapped Symbol and Label in ContactsAndFaults
 # 28 Sept 2020: Removed definitions of CSA feature classes, as this is handled in Create Database and in cross-section projection tools
+# 29 October 2020: Changed NoNulls to NullsOK for ObservedMapUnit in Stations per documentation - ET
 
 # to think about: Maybe change all NoNulls to NullsOK?
 
@@ -130,7 +131,7 @@ startDict = {
 					['Notes','String','Optional',defaultLength]],
 		   'Stations':         [['FieldID','String','NoNulls',IDLength],
                                         ['LocationConfidenceMeters','Single','NoNulls'],
-					['ObservedMapUnit','String','NoNulls',mapUnitLength],
+					['ObservedMapUnit','String','NullsOK',mapUnitLength],
 					['MapUnit','String','NoNulls',mapUnitLength],
 					['Symbol','String','NullsOK',defaultLength],
 					['Label','String','NullsOK',IDLength],
