62f58ef2d3ef56e17878a9fabeb360bec14c7478
Evan Thoms <ethoms@usgs.gov>
2021-04-14 15:24:13 -0800

    Rebuilt Map Units ready

--- Scripts/GeMS_RebuildMapUnits_Arc10.py
+++ Scripts/GeMS_RebuildMapUnits_Arc10.py
@@ -1,7 +1,22 @@
+"""GeMS_RebuildMapUnits
+A simpler version of the GeMS Make Polys tool
+1) Will not build MapUnitPolys from scratch. Use the ArcGIS Feature to Polygon tool for that.
+2) Does not concatenate a label points feature class from map units converted to points
+   and an optional label points feature class. It uses one or the other.
+3) No error checking between polygon and optional label points attributes. 
+   Use colored label points for that.
+   
+Use this tool in ArcMap while editing ContactsAndFaults linework to quickly rebuild the 
+MapUnitPolygons feature class as you change the shape of polygons or want to add new ones.
+"""
+
 import arcpy
 import os
 import sys
 import re
+from GeMS_utilityFunctions import *
+
+versionString = 'GeMS_MakePolys_Arc10.py, version of 14 April 2021'
 
 def get_trailing_number(s):
     m = re.search(r'\d+$', s)
@@ -9,20 +24,19 @@ def get_trailing_number(s):
 
 def findLyr(lname):
     mxd = arcpy.mapping.MapDocument('CURRENT')
-    for df in arcpy.mapping.ListDataFrames(mxd):
-        lList = arcpy.mapping.ListLayers(mxd, '*', df)
-        for lyr in lList:
-            if lyr.name == lname:
-                pos = lList.index(lyr)
-                #wks = lyr.workspacePath
-                if pos == 0:
-                    refLyr = lList[pos + 1]
-                    insertPos = "BEFORE"
-                else:
-                    refLyr = lList[pos - 1]
-                    insertPos = "AFTER"
-                    
-                return [lyr, df, refLyr, insertPos]
+    #for df in arcpy.mapping.ListDataFrames(mxd):
+    lList = arcpy.mapping.ListLayers(mxd, '*', mxd.activeDataFrame)
+    for lyr in lList:
+        if lyr.longName == lname:
+            pos = lList.index(lyr)
+            if pos == 0:
+                refLyr = lList[pos + 1]
+                insertPos = "BEFORE"
+            else:
+                refLyr = lList[pos - 1]
+                insertPos = "AFTER"
+                
+            return [lyr, mxd.activeDataFrame, refLyr, insertPos]
 
 def pm(msg, severity=0): 
 	# prints msg to screen and adds msg to the geoprocessor (in case this is run as a tool) 
@@ -48,15 +62,17 @@ saveMUP = arcpy.GetParameterAsText(3)
 
 #collect the findLyr properties
 lyrProps = findLyr(polyLayer)
-lyr = lyrProps[0]               #the layer object
-df = lyrProps[1]                #the data frame within which the layer resides
-refLyr = lyrProps[2]            #a layer above or below which the layer resides
-insertPos = lyrProps[3]         #index above or below the reference layer
-newPolys = lyr.dataSource       #the path to the dataSource of the polygon layer
+lyr = lyrProps[0]                               #the layer object
+df = lyrProps[1]                                #the data frame within which the layer resides
+refLyr = lyrProps[2]                            #a layer above or below which the layer resides
+insertPos = lyrProps[3]                         #index above or below the reference layer
+newPolys = lyr.dataSource                       #the path to the dataSource of the polygon layer
+discName = os.path.basename(lyr.dataSource)     #the name in the geodatabase of the datasource
 
-#save a temporary layer file for the polygons to save rendering and other settings
-#including joins to other tables
-#.lyr is saved to the folder of the geodatabase
+# save a temporary layer file for the polygons to save rendering and other settings
+# including joins to other tables
+# .lyr is saved to the folder of the geodatabase
+# lyr.workspacePath returnd the gdb, not a feature dataset
 lyrPath = os.path.join(os.path.dirname(lyr.workspacePath), lyr.name + '.lyr')
 if arcpy.Exists(lyrPath):
     os.remove(lyrPath)
@@ -78,14 +94,15 @@ except:
 # make a labelPoints feature class if one was not provided
 if labelPoints in ['#', '', None]:
     #create the labelPoints name
-    labelPoints = polyLayer + '_tempLabels'
+    labelPoints = discName + '_tempLabels'
+    testAndDelete(labelPoints)
 
     #check for an old copy of labelpoints
     if arcpy.Exists(labelPoints):
         arcpy.Delete_management(labelPoints)
-        
+    
     #create points from the attributed polygons
-    arcpy.FeatureToPoint_management(polyLayer, labelPoints, 'INSIDE')
+    arcpy.FeatureToPoint_management(lyr.dataSource, labelPoints, 'INSIDE')
 
 #and now remove the layer from the map
 arcpy.mapping.RemoveLayer(df, lyr)
@@ -93,7 +110,7 @@ arcpy.mapping.RemoveLayer(df, lyr)
 #save a copy of the polygons fc or delete
 if saveMUP == 'true':
     # get new name
-    pfcs = arcpy.ListFeatureClasses(polyLayer + "*", "Polygon")
+    pfcs = arcpy.ListFeatureClasses(discName + "*", "Polygon")
     maxN = 0
     for pfc in pfcs:
         try:
@@ -132,6 +149,5 @@ arcpy.SelectLayerByAttribute_management(lineLayer, "CLEAR_SELECTION")
 # add the layer file 
 pm("  adding " + lyrPath + " to the map")
 addLyr = arcpy.mapping.Layer(lyrPath)
-#arcpy.mapping.AddLayer(df, addLyr)
 arcpy.mapping.InsertLayer(df, refLyr, addLyr, insertPos)
 #arcpy.Delete_management(lyrPath)
