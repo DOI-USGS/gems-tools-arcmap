2ad6ac5628e3fc7662c5866ed40c4ba2994d143f
Evan Thoms <ethoms@usgs.gov>
2021-05-13 11:31:22 -0800

    new versionString of ValidateDatabase

--- Scripts/GeMS_ValidateDatabase_Arc10.py
+++ Scripts/GeMS_ValidateDatabase_Arc10.py
@@ -31,7 +31,7 @@ from GeMS_utilityFunctions import *
 from GeMS_Definition import *
 import copy
 
-versionString = 'GeMS_ValidateDatabase_Arc10.py, version of 8 March 2021'
+versionString = 'GeMS_ValidateDatabase_Arc10.py, version of 5 May 2021'
 rawurl = 'https://raw.githubusercontent.com/usgs/gems-tools-arcmap/master/Scripts/GeMS_ValidateDatabase_Arc10.py'
 checkVersion(versionString, rawurl, 'gems-tools-arcmap')
 
