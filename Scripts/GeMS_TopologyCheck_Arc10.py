aebf78665801f3f51a73d2715892a87a6ce144fa
Evan Thoms <ethoms@usgs.gov>
2021-08-13 11:02:36 -0800

    TopologyCheck - field length in errors gdb increased to 500

--- Scripts/GeMS_TopologyCheck_Arc10.py
+++ Scripts/GeMS_TopologyCheck_Arc10.py
@@ -47,7 +47,7 @@ Significant assumptions:
 import arcpy, os, sys, math, os.path, operator, time
 from GeMS_utilityFunctions import *
 
-versionString = 'GeMS_TopologyCheck_Arc10.py, version of 13 AugustS 2021'
+versionString = 'GeMS_TopologyCheck_Arc10.py, version of 13 August 2021'
 rawurl = 'https://raw.githubusercontent.com/usgs/gems-tools-arcmap/master/Scripts/GeMS_TopologyCheck_Arc10.py'
 checkVersion(versionString, rawurl, 'gems-tools-arcmap')
 # see gems-tools version<=1.3 to get earlier TopologyCheck tool
